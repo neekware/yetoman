11a2f84e557fb516b61c57b9f3dd01eea1463889
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2022-11-28 11:42:15 -0500

    add subscribe, standalone custom server

--- apps/yetoman/pages/api/gql.ts
+++ apps/yetoman/pages/api/gql.ts
@@ -1,27 +1,3 @@
-import "reflect-metadata";
-
-import { ApolloServer } from "apollo-server-micro";
-import { buildSchema } from "type-graphql";
-
-import { UsersResolver } from "../../schema/user/user.resolver";
-
-const schema = await buildSchema({
-  resolvers: [UsersResolver],
-});
-
-const server = new ApolloServer({
-  schema,
-});
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const startServer = server.start();
-
-export default async function handler(req, res) {
-  await startServer;
-  await server.createHandler({ path: '/api/gql' })(req, res);
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' });
 }
