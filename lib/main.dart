fc3f2c8aaf423e5256530a9696af61277dd26ab7
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-03-28 23:05:35 -0400

    in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,19 +7,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Builder(
-        builder: (context) => Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Colors.transparent, // Parent container is transparent
-            child: const Center(
-              child: Text(
-                'Hello, World!',
-                style: TextStyle(
-                    fontSize: 24, color: Colors.blue), // Child text is opaque
-              ),
+    final size = MediaQuery.of(context).size;
+    return CupertinoApp(
+      home: Center(
+        child: Container(
+          color: const Color(0x00000000),
+          width: size.width,
+          height: size.height,
+          child: const Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: CupertinoColors.activeBlue), // Child text is opaque
             ),
           ),
         ),
