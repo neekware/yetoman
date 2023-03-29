33c5da58442e7b0a12dbc99b8fa16176175f8cad
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-03-29 13:00:31 -0400

    mac utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:macos_window_utils/macos_window_utils.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WindowManipulator.initialize();
+  WindowManipulator.makeTitlebarTransparent();
+  WindowManipulator.enableFullSizeContentView();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: FractionallySizedBox(
-          widthFactor: 1,
-          heightFactor: 1,
-          child: Opacity(
-            opacity: 0.1,
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      width: 0,
-                      color: Colors
-                          .transparent), //color is transparent so that it does not blend with the actual color specified
-                  color: const Color.fromARGB(255, 34, 208, 1)),
-              // color: const Color(0x00000000),
-              width: 100,
-              height: 500,
-              child: const Center(
-                child: Text(
-                  'Hello, World!',
-                  style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.blueAccent), // Child text is opaque
+    return TitlebarSafeArea(
+      child: MaterialApp(
+        home: Center(
+          child: FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 1,
+            child: Opacity(
+              opacity: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        width: 0,
+                        color: Colors
+                            .transparent), //color is transparent so that it does not blend with the actual color specified
+                    color: const Color.fromARGB(255, 34, 208, 1)),
+                // color: const Color(0x00000000),
+                width: 100,
+                height: 500,
+                child: const Center(
+                  child: Text(
+                    'Hello, World!',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.blueAccent), // Child text is opaque
+                  ),
                 ),
               ),
             ),
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -11,22 +11,6 @@ class MainFlutterWindow: NSWindow {
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
-    // make window fullscreen and remove title bar
-    // styleMask.insert(.fullScreen)
-    // styleMask.remove(.titled)
-    // // make window always on top
-    level = .floating
-    // make window transparent
-    isOpaque = false
-    backgroundColor = NSColor.clear
-    // set transparncy to 0.5
-    alphaValue = 0.5
-    // // make windown click through
-    // isMovableByWindowBackground = true
-    // // make window borderless
-    styleMask.insert(.borderless)
-    // // make window not resizable
-    styleMask.remove(.resizable)
-    
+   
   }
 }
