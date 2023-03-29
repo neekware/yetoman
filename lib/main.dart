39f27e5bcb0a3aaf60be88fd024d780c549f04ae
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-03-29 12:28:57 -0400

    mods

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,19 +7,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return CupertinoApp(
+    return MaterialApp(
       home: Center(
-        child: Container(
-          color: const Color(0x00000000),
-          width: size.width,
-          height: size.height,
-          child: const Center(
-            child: Text(
-              'Hello, World!',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: CupertinoColors.activeBlue), // Child text is opaque
+        child: FractionallySizedBox(
+          widthFactor: 1,
+          heightFactor: 1,
+          child: Opacity(
+            opacity: 0.1,
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      width: 0,
+                      color: Colors
+                          .transparent), //color is transparent so that it does not blend with the actual color specified
+                  color: const Color.fromARGB(255, 34, 208, 1)),
+              // color: const Color(0x00000000),
+              width: 100,
+              height: 500,
+              child: const Center(
+                child: Text(
+                  'Hello, World!',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.blueAccent), // Child text is opaque
+                ),
+              ),
             ),
           ),
         ),
